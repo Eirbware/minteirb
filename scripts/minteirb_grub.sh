89fe239c829f98fce09d4906ac8ddce7a423cffe
Artémystère <code@artemis.simplelogin.com>
2026-05-01 21:19:27 +0200

    feat: better grub minteirb integration

--- scripts/minteirb_grub.sh
+++ scripts/minteirb_grub.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ### Changing the name in grub
+sed -i -e 's/Ubuntu/Minteirb/g' /etc/default/grub
+update-grub
 sed -i -e 's/Linux Mint/Minteirb/g' /boot/grub/grub.cfg
+sed -i -e 's/Ubuntu/Minteirb/g' /boot/grub/grub.cfg
 ### Removing splash screen (mint logo during boot)
 sed -i -e 's/splash//' /boot/grub/grub.cfg
