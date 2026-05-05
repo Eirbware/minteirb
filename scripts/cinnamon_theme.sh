0cf7a184afd77ebc8e7537535bf05310d2a88333
Artémystère <code@artemis.simplelogin.com>
2026-05-05 11:20:17 +0200

    clean: minteirb logo

--- scripts/cinnamon_theme.sh
+++ scripts/cinnamon_theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### Changing cinnamon default background and theme
-gsettings set org.cinnamon.desktop.background picture-uri file:///usr/share/backgrounds/minteirb_wallpaper.png
+gsettings set org.cinnamon.desktop.background picture-uri file:///usr/share/backgrounds/minteirb.png
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Sand'
 gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Sand'
 gsettings set org.cinnamon.desktop.interface font-name 'Noto Sans 10'
