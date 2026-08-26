fffd8755edb3bb9ec5aa974688f9e511705d2f16
Artémystère <contact+dev@artemystere.fr>
2026-08-26 17:12:14 +0200

    feat: typo + improvement

--- scripts/minteirb_grub.sh
+++ scripts/minteirb_grub.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# Make sure that Minteirb is show in the menu (and not Ubuntu or Linux Mint)
+# Make sure that Minteirb is shown in the menu (and not Ubuntu or Linux Mint)
 sed -i -e 's/Linux Mint/Minteirb/g' /boot/grub/grub.cfg
+sed -i -e 's/Cinnamon//g' /boot/grub/grub.cfg
 sed -i -e 's/Ubuntu/Minteirb/g' /boot/grub/grub.cfg
